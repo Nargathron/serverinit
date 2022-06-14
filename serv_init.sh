f15f16b58b3e9b046434d8d99d54b206449e3a8f
borgyy <borgyy@borgyysair.local>
2022-06-14 14:34:45 +0300

    fix: change sudo to starship install

--- serv_init.sh
+++ serv_init.sh
@@ -3,4 +3,4 @@ sudo yum -y update && sudo yum -y install zsh git vim mlocate mc wget curl epel-
 yes y | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-yes y | curl -sS https://starship.rs/install.sh | sh
+yes y | sudo curl -sS https://starship.rs/install.sh | sh
