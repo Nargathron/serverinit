f8af473c4d366cdd969a216c0ae2748d7e960fdd
borgyy <borgyy@borgyysair.local>
2022-06-23 22:36:57 +0300

    fix: download sh

--- serv_init.sh
+++ serv_init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 yum -y update && sudo yum -y install zsh git vim mlocate mc wget curl epel-release
-yes y | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-yes y | curl -sS https://starship.rs/install.sh | sh
+wget https://starship.rs/install.sh
+chmod +x install.sh
+./install.sh --yes
