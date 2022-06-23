f5a086a4051c5c38d389b44a327fbc95da74b501
borgyy <borgyy@borgyysair.local>
2022-06-23 23:30:35 +0300

    fix: add wget to oh-my-zsh

--- serv_init.sh
+++ serv_init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 yum -y update && sudo yum -y install zsh git vim mlocate mc wget curl epel-release
 chsh -s /bin/zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+./install.sh --yes
+rm install.sh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 wget https://starship.rs/install.sh
